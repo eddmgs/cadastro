1dbc52973709b8a5098d587a4a58e0237c790e60
Edson Filho <filhoedson15@gmail.com>
2026-03-23 11:27:44 -0300

    Initial commit

---
+++ src/test/java/eumesmo/Cadastrosimples/CadastrosimplesApplicationTests.java
@@ -0,0 +1,13 @@
+package eumesmo.Cadastrosimples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastrosimplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
